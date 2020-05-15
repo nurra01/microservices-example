6a1d863360629cc21650a017345dadb01b4ef7ce
Rauan Nurbatyrov <rauan_nurbatyrov@mckinsey.com>
2020-05-15 17:25:08 +0200

    add not found page

--- client/src/App.js
+++ client/src/App.js
@@ -1,82 +1,16 @@
 import React from 'react';
-import { TextField, Button } from "@material-ui/core"
-import useInputState from "./hooks/useInputState"
-import axios from "axios"
-import './App.css';
+import { Redirect, Route, Switch, BrowserRouter } from 'react-router-dom'
+import { Register, NotFound } from './components';
 
 function App() {
-  const [firstName, handleFirstNameChange] = useInputState("")
-  const [lastName, handleLastNameChange] = useInputState("")
-  const [email, handleEmailChange] = useInputState("")
-
-  const handleSubmit = (event) => {
-    axios.post("http://localhost:8080/user/register", {
-      firstName,
-      lastName,
-      email
-    }, {
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      }
-    }).then(val => {
-      console.log(val)
-      alert(val.statusText)
-    }).catch(err => {
-      console.error(err.response)
-      if (err.response) {
-        alert(err.response.data.message)
-      }
-    })
-    event.preventDefault();
-  }
-
   return (
-    <div
-      className="App"
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <h1>Welcome to microservices with kafka example</h1>
-      <form
-        onSubmit={handleSubmit}
-        noValidate
-        autoComplete="off"
-        style={{
-          width: '40%',
-          display: 'flex',
-          flexDirection: 'column'
-        }}>
-        <TextField
-          label="First name"
-          value={firstName}
-          onChange={handleFirstNameChange}
-        />
-        <TextField
-          label="Last name"
-          value={lastName}
-          onChange={handleLastNameChange}
-        />
-        <TextField
-          label="Email"
-          value={email}
-          onChange={handleEmailChange}
-        />
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          style={{
-            width: '100px',
-            margin: '20px auto'
-          }}>
-          Sign up
-        </Button>
-      </form>
-    </div >
-  );
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Register} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ client/src/components/NotFound.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { TextField, Button } from "@material-ui/core"
+
+function NotFound(props) {
+    const handleRedirect = () => {
+        props.history.push("/")
+    }
+    return (
+        <div className="not-found">
+            <div className="not-found-content">
+                <h1 id="title">Ooops!</h1>
+                <h1 id="message">404 - PAGE NOT FOUND</h1>
+                <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    onClick={handleRedirect}>
+                    Go to homepage
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+export default NotFound
