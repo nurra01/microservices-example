4bb7f94f29509798d62b13c0886717f99f43ba14
Rauan Nurbatyrov <rauan_nurbatyrov@mckinsey.com>
2020-05-24 14:05:45 +0200

    add password field to save in DB

--- services/user-storage/db/user.go
+++ services/user-storage/db/user.go
@@ -7,7 +7,7 @@ import (
 
 // SaveNewUser saves a user in the DB
 func SaveNewUser(user *models.RegisterUser) error {
-	_, err := dbConn.NamedExec(`INSERT INTO users (id,firstName,lastName,email,verified) VALUES (:id,:firstName,:lastName,:email,:verified)`, user)
+	_, err := dbConn.NamedExec(`INSERT INTO users (id,first_name,last_name,email,password,verified) VALUES (:id,:first_name,:last_name,:email,:password,:verified)`, user)
 	if err != nil {
 		return fmt.Errorf("failed to save a new user. Error: %v", err.Error())
 	}
@@ -18,9 +18,10 @@ func SaveNewUser(user *models.RegisterUser) error {
 func SetupTable() error {
 	_, err := dbConn.Exec(`CREATE TABLE IF NOT EXISTS users (
 		id VARCHAR(100) NOT NULL PRIMARY KEY,
-		firstName VARCHAR(100) NOT NULL,
-		lastName VARCHAR(100) NOT NULL,
+		first_name VARCHAR(100) NOT NULL,
+		last_name VARCHAR(100) NOT NULL,
 		email VARCHAR(100) NOT NULL,
+		password VARCHAR(255) NOT NULL,
 		verified BOOLEAN NOT NULL
 	)`)
 	if err != nil {
--- services/user-storage/models/user.go
+++ services/user-storage/models/user.go
@@ -3,8 +3,9 @@ package models
 // RegisterUser defines type for user signup
 type RegisterUser struct {
 	ID        string `json:"id" db:"id"`
-	FirstName string `json:"firstName" db:"firstName"`
-	LastName  string `json:"lastName" db:"lastName"`
+	FirstName string `json:"firstName" db:"first_name"`
+	LastName  string `json:"lastName" db:"last_name"`
 	Email     string `json:"email" db:"email"`
+	Password  string `json:"password" db:"password"`
 	Verified  bool   `json:"verified" db:"verified"`
 }
